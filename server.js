ea23d2ce99df56ffa95e6adb1c6aa345fb928b2c
Jacob Lockhart <procoder165@gmail.com>
2022-09-23 20:08:15 -0400

    getting started

---
+++ server.js
@@ -0,0 +1,7 @@
+const path = require("path");
+const express = require("express");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+const app = express();
+const PORT = proces.env.PORT || 3001;
+const sequelize = require("./config/connection");
