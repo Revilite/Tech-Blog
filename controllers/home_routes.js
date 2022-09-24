4a01ca4cbf78f33382357e05ef07087bff94a149
Jacob Lockhart <procoder165@gmail.com>
2022-09-23 20:59:42 -0400

    set up main page and login page

--- controllers/home_routes.js
+++ controllers/home_routes.js
@@ -1,6 +1,11 @@
 const home  = require("express").Router();
 
 
+
+home.get("/login", async(req, res) =>{
+    res.render("login")
+})
+
 home.get("*", async (req, res) =>{
     console.log("get requested");
     res.render('all');
