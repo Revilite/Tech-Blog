58cebb24065962e11dfe2afab63b4838010294ac
Jacob Lockhart <procoder165@gmail.com>
2022-09-25 19:13:22 -0400

    adding posts to html

--- controllers/dash_routes.js
+++ controllers/dash_routes.js
@@ -6,5 +6,18 @@ dash.get("/", async (req, res) =>{
         res.render("dash");
 })
 
+dash.post("/", async (req, res) =>{
+        try{
+                const data = await models.Post.create({
+                        title: req.body.title,
+                        body: req.body.body,
+                });
+                res.status(200).json(data);
+        }
+        catch(err){
+                res.status(500).json(err);
+                throw err;
+        }
+})
 
 module.exports = dash;
--- public/js/dash.js
+++ public/js/dash.js
@@ -1,14 +1,43 @@
 const newButton = document.querySelector("#newButton");
-window.alert("Hello World");
+const newPost = document.querySelector("#newPost");
+const submitButton = document.querySelector("#submit");
 
 
- async function newPostHandler(event){
+
+async function showBlock(event){
     event.preventDefault();
-    window.alert("Hello World");
-    const newPost = document.querySelector("#newPost");
-    newPost.s
+    if(newPost.classList.contains("silent")){
+    newPost.classList.remove("silent");
+    }
+    else{
+        newPost.classList.add("silent");
+    }
+}
+
+async function formHandeler(event){
+    event.preventDefault();
+    const title = document.querySelector("#title").value;
+    const body = document.querySelector("#body").value;
+
+    const response = await fetch("/dashboard", {
+        method: "POST",
+        body: JSON.stringify({
+            title,
+            body,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+    });
+    if (response.ok) {
+        document.location.replace("/dashboard");
+    }
+    else{
+        window.alert("Failed to add post");
+    }
 }
 
 
 
-newButton.addEventListener("submit", newPostHandler);
+newButton.addEventListener("click", showBlock);
+submitButton.addEventListener("submit", formHandeler);
