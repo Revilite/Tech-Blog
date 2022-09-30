1297d365a350c01ccb1ce0c1cdba2d2a093e6514
Jacob Lockhart <procoder165@gmail.com>
2022-09-29 20:49:01 -0400

    added boolean trigger

--- controllers/api/user_routes.js
+++ controllers/api/user_routes.js
@@ -16,7 +16,13 @@ users.get("/", async (req, res) =>{
 })
 
 users.post("/", async (req, res) =>{  // Login
-    
+    const updatedData = await models.User.update({isOnline : true},{
+        where: {
+            username: req.body.username,
+        }
+    })
+
+
     const data = await models.User.findOne({ where: { username: req.body.username } });
         if(!data){
             res.status(400)
@@ -40,6 +46,12 @@ users.post("/", async (req, res) =>{  // Login
 
 users.post("/logout", async (req,res) =>{
 if(req.session.isOnline){
+    const updatedData = await models.User.update({isOnline: false}, {
+        where: {
+            isOnline: true,
+        }
+    })
+
     req.session.destroy(() =>{
         res.status(204).end();
     });
